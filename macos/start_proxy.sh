fb29e9566bf9f716adc61234813dddd9ef515a20
rA3ka <160638097+rA3ka@users.noreply.github.com>
2026-05-27 23:05:00 +0200

    Update start_proxy.sh
    Use local tempdir instead and do not cleanup, this will result in faster bootstrap when cached-descriptors are available.
    
    Signed-off-by: rA3ka <160638097+rA3ka@users.noreply.github.com>

--- macos/start_proxy.sh
+++ macos/start_proxy.sh
@@ -17,7 +17,6 @@ if [[ "$(sw_vers --productName)" == "macOS" ]];then
 		
 		kill $(pgrep anon) > /dev/null 2>&1		
 		rm anon anonrc
-		rm -r ./proxy-data
 		
 		echo -e "\n${BLUE_ANON}======================================================${NOCOLOR}"
 		echo -e "${RED}                 ANON Proxy terminated                       ${NOCOLOR}"
@@ -33,9 +32,8 @@ if [[ "$(sw_vers --productName)" == "macOS" ]];then
 	
 	unzip -o anon-live-macos-$(uname -p)64.zip anon > /dev/null 2>&1	
 	kill $(pgrep anon) > /dev/null 2>&1
-	mkdir ./proxy-data
-	curl -s https://dns.ec.anyone.tech/tld/anyone > ./proxy-data/anyone_hosts
-	echo -e "SocksPort 127.0.0.1:9055\nSocksPolicy accept 127.0.0.1\nSocksPolicy reject *\nHTTPTunnelPort auto\nDataDirectory ./proxy-data" > anonrc	
+	curl -s https://dns.ec.anyone.tech/tld/anyone > ~/.anon/anyone_hosts
+	echo -e "SocksPort 127.0.0.1:9055\nSocksPolicy accept 127.0.0.1\nSocksPolicy reject *\nHTTPTunnelPort auto\nDataDirectory ~/.anon" > anonrc	
 	./anon -f anonrc --agree-to-terms | grep "Bootstrapped" &	
 	sleep 1	
 	networksetup -setsocksfirewallproxy "Wi-Fi" 127.0.0.1 9055
